578a6ac615931e95437930221b6790f9dc523cc3
SeifMostafa <seifmostafa235@gmail.com>
2017-05-10 17:20:27 +0200

    motor and main

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
 #include "opencv2/objdetect/objdetect.hpp"
-<<<<<<< HEAD:main.cpp
 #include "opencv2/videoio.hpp"
-=======
->>>>>>> bf1167475bf0bfadb46c27fea33526e9f900d159:facedetect_runservo.cpp
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
-#include <vector>
 #include <stdio.h>
-
-#include <wiringPi.h>
-#include <softPwm.h>
-
 using namespace std;
 using namespace cv;
-
 void detectAndDisplay( Mat frame );
 String body_cascade_name = "haarcascade_mcs_upperbody.xml";
 String frontalFace_cascade_name = "haarcascade_frontalface_alt.xml";
@@ -23,52 +14,17 @@ CascadeClassifier body_cascade;
 CascadeClassifier frontalFace_cascade;
 CascadeClassifier profileFace_cascade;
 String window_name = "Capture - Face detection";
-
-void RunServo(int pos){
-wiringPiSetup();
-pinMode(1,PWM_OUTPUT);
-int c=0;
-while(c<pos)
-{
-pwmWrite(1,1);
-}
-
-}
-Mat3b binding(Mat img1,Mat img2){
-
-    // Get dimension of final image
-    int rows = max(img1.rows, img2.rows);
-    int cols = img1.cols + img2.cols;
-
-    // Create a black image
-    Mat3b res(rows, cols);
-
-    // Copy images in correct position
-    img1.copyTo(res(Rect(0, 0, img1.cols, img1.rows)));
-    img2.copyTo(res(Rect(img1.cols, 0, img2.cols, img2.rows)));
-return res;
-}
-void printcenters(vector<Point>centers){
-for(int u=0;u<centers.size();u++){
-cout<<centers.get(u);
-}
-}
 int main( void )
 {
-<<<<<<< HEAD:main.cpp
     Mat frame1,frame2;
     int width=640;
     double FOV=180,dpp=FOV/((double)width);
-=======
-    Mat frame,frame2;
->>>>>>> bf1167475bf0bfadb46c27fea33526e9f900d159:facedetect_runservo.cpp
     //-- 1. Load the cascades
     if( !body_cascade.load( body_cascade_name ) ){ printf("--(!)Error loading body cascade\n"); return -1; };
     if( !frontalFace_cascade.load( frontalFace_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
     if( !profileFace_cascade.load( profileFace_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
 
     VideoCapture capture;
-<<<<<<< HEAD:main.cpp
     capture.open(0);
     capture.open(1);
     while(1){
@@ -81,43 +37,14 @@ int main( void )
         frame2.copyTo(frame(Rect(frame1.cols, 0, frame2.cols, frame2.rows)));
 
         detectAndDisplay( frame );
-=======
-    while(1){
-        capture.open(0);
-	capture.read(frame);
- 	capture.open(1);
-	capture.read(frame2);
-
-        detectAndDisplay( binding(frame,frame2) );
->>>>>>> bf1167475bf0bfadb46c27fea33526e9f900d159:facedetect_runservo.cpp
         waitKey(250);
     }
     return 0;
 }
 void detectAndDisplay( Mat frame )
 {
-	vector<Point>Centers = new vector<Point>;
-
     std::vector<Rect> bodies;
     Mat frame_gray=Mat::zeros( frame.size(), frame.type() );
-<<<<<<< HEAD:main.cpp
-=======
-
-
-     /// Do the operation new_image(i,j) = alpha*image(i,j) + beta
-/* for( int y = 0; y < frame.rows; y++ )
-    { for( int x = 0; x < frame.cols; x++ )
-         { for( int c = 0; c < 3; c++ )
-              {
-      frame_gray.at<Vec3b>(y,x)[c] =
-         saturate_cast<uchar>( 2*( frame.at<Vec3b>(y,x)[c] ) +30 );
-             }
-    }
-    }
-    imshow("Modified",frame_gray);*/
-
-
->>>>>>> bf1167475bf0bfadb46c27fea33526e9f900d159:facedetect_runservo.cpp
     cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
     equalizeHist( frame_gray, frame_gray );
 
@@ -126,7 +53,6 @@ void detectAndDisplay( Mat frame )
     for ( size_t i = 0; i < bodies.size(); i++ )
     {
         Point center( bodies[i].x + bodies[i].width/2, bodies[i].y + bodies[i].height/2 );
-	Centers.push(center);
         ellipse( frame, center, Size( bodies[i].width/2, bodies[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
         Mat faceROI = frame_gray( bodies[i] );
         std::vector<Rect> faces;
@@ -151,16 +77,10 @@ void detectAndDisplay( Mat frame )
             angle(face_center.x);
         }
         }
-
     }
     //-- Show what you got
-	printcenters(Centers);
     imshow( window_name, frame );
 }
-<<<<<<< HEAD:main.cpp
 double angle(double x){
 return x*dpp;
 }
-=======
-
->>>>>>> bf1167475bf0bfadb46c27fea33526e9f900d159:facedetect_runservo.cpp
