f22bae3b1cddf0cab8148efaa841670618e9fc1f
Muhammad Aldakkak <muhammad.19931@gmail.com>
2017-06-13 06:50:29 +0200

    Main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,23 @@
 #include "opencv2/objdetect/objdetect.hpp"
-#include "opencv2/videoio.hpp"
+//#include "opencv2/videoio.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
-#include <fstream>
 #include <stdio.h>
+#include <vector>
 using namespace std;
 using namespace cv;
-void detectAndDisplay( Mat frame );
+std::vector<double> detectAndDisplay( Mat frame );
+double angle(double x);
 String body_cascade_name = "haarcascade_mcs_upperbody.xml";
 String frontalFace_cascade_name = "haarcascade_frontalface_alt.xml";
 String profileFace_cascade_name ="haarcascade_profileface.xml";
 CascadeClassifier body_cascade;
 CascadeClassifier frontalFace_cascade;
 CascadeClassifier profileFace_cascade;
-double angle(double x);
 String window_name = "Capture - Face detection";
-int width=640;
-double FOV=180,dpp=FOV/((double)width);
+    int width=640;
+    double FOV=180,dpp=FOV/((double)width);
 class Dbrw
 {
 public:
@@ -46,87 +46,78 @@ public:
         return result;
     };
 };
-
 int main( void )
 {
-     Mat frame1,frame2;
+    Mat frame1,frame2;
 
-     //-- 1. Load the cascades
-     if( !body_cascade.load( body_cascade_name ) )
-     {
-         printf("--(!)Error loading body cascade\n");
-         return -1;
-     };
-     if( !frontalFace_cascade.load( frontalFace_cascade_name ) )
-     {
-         printf("--(!)Error loading face cascade\n");
-         return -1;
-     };
-     if( !profileFace_cascade.load( profileFace_cascade_name ) )
-     {
-         printf("--(!)Error loading face cascade\n");
-         return -1;
-     };
+    //-- 1. Load the cascades
+    if( !body_cascade.load( body_cascade_name ) ){ printf("--(!)Error loading body cascade\n"); return -1; };
+    if( !frontalFace_cascade.load( frontalFace_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
+    if( !profileFace_cascade.load( profileFace_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
 
-     VideoCapture capture;
-     capture.open(0);
-     capture.open(1);
-     while(1)
-     {
-         capture.read(frame1);
-         capture.read(frame2);
-         int rows = max(frame1.rows, frame2.rows);
-         int cols = frame1.cols + frame2.cols;
-         Mat3b frame(rows, cols, Vec3b(0,0,0));
-         frame1.copyTo(frame(Rect(0, 0, frame1.cols, frame1.rows)));
-         frame2.copyTo(frame(Rect(frame1.cols, 0, frame2.cols, frame2.rows)));
+    VideoCapture capture;
+    while(1){
+        capture.open(0);
 
-         detectAndDisplay( frame );
-         waitKey(250);
-     }
-     return 0;
+		capture.read(frame1);
+		capture.release();
+        capture.open(1);
+		capture.read(frame2);
+        capture.release();
+        int rows = max(frame1.rows, frame2.rows);
+        int cols = frame1.cols + frame2.cols;
+        Mat3b frame(rows, cols, Vec3b(0,0,0));
+        frame1.copyTo(frame(Rect(0, 0, frame1.cols, frame1.rows)));
+        frame2.copyTo(frame(Rect(frame1.cols, 0, frame2.cols, frame2.rows)));
 
+        detectAndDisplay( frame );
+        waitKey(250);
+    }
+    return 0;
 }
-void detectAndDisplay( Mat frame )
+std::vector<double> detectAndDisplay( Mat frame )
 {
     std::vector<Rect> bodies;
     Mat frame_gray=Mat::zeros( frame.size(), frame.type() );
     cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
     equalizeHist( frame_gray, frame_gray );
-
+    std::vector<double>facesAngles;
     //-- Detect bodies
-    body_cascade.detectMultiScale( frame_gray, bodies, 1.2, 4, 0|CASCADE_SCALE_IMAGE, Size(70, 300) );
+    body_cascade.detectMultiScale( frame_gray, bodies, 1.1, 4, 0|CASCADE_SCALE_IMAGE, Size(40,60) );
     for ( size_t i = 0; i < bodies.size(); i++ )
     {
         Point center( bodies[i].x + bodies[i].width/2, bodies[i].y + bodies[i].height/2 );
         ellipse( frame, center, Size( bodies[i].width/2, bodies[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
         Mat faceROI = frame_gray( bodies[i] );
         std::vector<Rect> faces;
+        facesAngles.push_back(angle(center.x));
         //-- In each body, detect faces
         bool check=true;
         frontalFace_cascade.detectMultiScale( faceROI, faces, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(10, 30) );
+        if(faces.size()>0) facesAngles.pop_back();
         for ( size_t j = 0; j < faces.size(); j++ )
         {
             check=false;
             Point face_center( bodies[i].x + faces[j].x + faces[j].width/2, bodies[i].y + faces[j].y + faces[j].height/2 );
             int radius = cvRound( (faces[j].width + faces[j].height)*0.25 );
             circle( frame, face_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-            angle(face_center.x);
+            facesAngles.push_back(angle(face_center.x));
         }
-        if(check)
+        if(check){
+        profileFace_cascade.detectMultiScale( faceROI, faces, 1.1, 6, 0 |CASCADE_SCALE_IMAGE, Size(10, 30) );
+        if(faces.size()>0) facesAngles.pop_back();
+        for ( size_t j = 0; j < faces.size(); j++ )
         {
-            profileFace_cascade.detectMultiScale( faceROI, faces, 1.1, 6, 0 |CASCADE_SCALE_IMAGE, Size(10, 30) );
-            for ( size_t j = 0; j < faces.size(); j++ )
-            {
-                Point face_center( bodies[i].x + faces[j].x + faces[j].width/2, bodies[i].y + faces[j].y + faces[j].height/2 );
-                int radius = cvRound( (faces[j].width + faces[j].height)*0.25 );
-                circle( frame, face_center, radius, Scalar( 255, 255, 0 ), 4, 8, 0 );
-                angle(face_center.x);
-            }
+            Point face_center( bodies[i].x + faces[j].x + faces[j].width/2, bodies[i].y + faces[j].y + faces[j].height/2 );
+            int radius = cvRound( (faces[j].width + faces[j].height)*0.25 );
+            circle( frame, face_center, radius, Scalar( 255, 255, 0 ), 4, 8, 0 );
+            facesAngles.push_back(angle(face_center.x));
+        }
         }
     }
     //-- Show what you got
     imshow( window_name, frame );
+    return facesAngles;
 }
 double angle(double x)
 {
